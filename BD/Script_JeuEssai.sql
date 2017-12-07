1c57289dfcbd16b153f3b0f9bec348963ef2878b
Quentin Bouny <bouny@info26-13.iut45.univ-orleans.fr>
2017-12-07 11:44:45 +0100

    Création jeu d'essai

---
+++ BD/Script_JeuEssai.sql
@@ -0,0 +1,29 @@
+insert into PERSONNE values(0,"BOUNY","Quentin");
+insert into PERSONNE values(1,"COUCKE","Maxime");
+insert into PERSONNE values(2,"CAMA","Thang-Long");
+insert into PERSONNE values(3,"TAMIN","Antoine");
+insert into PERSONNE values(4,"FAURE","Nathan");
+
+
+insert into ELEVE values(4);
+insert into ELEVE values(3);
+insert into ELEVE values(2);
+
+
+insert into PROFESSEUR values(0);
+insert into PROFESSEUR values(1);
+
+insert into GROUPE values(1,"STMG","Trisomie","Hôpital Psychatrique d'Orléans","");
+
+insert into APPARTIENT_AU values(4,1);
+insert into APPARTIENT_AU values(3,1);
+insert into APPARTIENT_AU values(2,1);
+
+insert into JURY values(1,"Login","MotDePasse");
+
+insert into APPARTIENT_A values(0,1);
+insert into APPARTIENT_A values(1,1);
+
+insert into HEURE values(1,STR_TO_DATE('1020', '%h%i'),STR_TO_DATE('1040', '%h%i'));
+
+insert into JUGE values(1,1,1);
