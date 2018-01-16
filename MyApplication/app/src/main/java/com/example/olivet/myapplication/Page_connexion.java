318bd9ba4f6f1000fe24786d0eccc159b6caeecb
Martin Schultz <martin.schultz@etu.univ-orleans.fr>
2018-01-16 17:31:55 +0100

    Début création test BD (bug)

--- MyApplication/app/src/main/java/com/example/olivet/myapplication/Page_connexion.java
+++ MyApplication/app/src/main/java/com/example/olivet/myapplication/Page_connexion.java
@@ -2,20 +2,27 @@ package com.example.olivet.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import static com.example.olivet.myapplication.JuryManager.KEY_LOGIN_;
+import static com.example.olivet.myapplication.JuryManager.KEY_NUMJURY;
 
 /**
  * Created by olivet on 21/12/17.
  */
 
 public class Page_connexion extends Activity {
-    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_connexion);
+
+        final EditText identifiant = (EditText)findViewById(R.id.EditTextIdentifiant);
 
         Button button = (Button)findViewById(R.id.button2);
 
@@ -23,7 +30,18 @@ public class Page_connexion extends Activity {
             @Override
             public void onClick(View view) {
                 finish();
-                startActivity(new Intent(Page_connexion.this,Planning.class));
+                Intent i = new Intent(Page_connexion.this,Planning.class);
+                JuryManager juryMan = new JuryManager(view.getContext());
+                Cursor jurys = juryMan.getJurys();
+                int id = 0;
+                if (jurys.moveToFirst()) {
+                    if (jurys.getString(jurys.getColumnIndex(KEY_LOGIN_)).equals(identifiant.getText().toString())) {
+                        id = jurys.getInt(jurys.getColumnIndex(KEY_NUMJURY));
+                    }
+                    jurys.close();
+                }
+                i.putExtra("NumJury", id);
+                startActivity(i);
             }
         });
     }
