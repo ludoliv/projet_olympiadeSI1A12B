5f86f77965fe4b8e6e5909b7b675120783a09a3c
Quentin Bouny <bouny@info24-02.iut45.univ-orleans.fr>
2018-01-17 11:56:44 +0100

    Modif fonctions dans Interactions

--- BD/Interactions/ImportCSV.php
+++ BD/Interactions/ImportCSV.php
@@ -1,37 +1,35 @@
 <?php
 
-function getCSVForEleve($connexion,$filename){
-    $row = 1;
+function getCSVforEleve($connexion,$filename)
+{
     $handle = fopen($filename,"r");
+    $tabPersonne = array();
     $tabE = array();
     $tabP = array();
     $id = getMaxIDPersonne($connexion)+1;
     while(($data = fgetcsv($handle, 1000, ",")) !== FALSE)
     {
-         $num = count($data);
-         for ($c=0; $c < $num; $c++)
-         {
-           $p = new Personne($id,$data[0],$data[1]);
-           $e = new Eleve($id,$data[2],$data[3]);
-           if (test($tabP,$id))
-           {
-             break;
-           }
-           else
-           {
+        $Nom = $data[0];
+        $Prenom = $data[1];
+        $Filiere = $data[2];
+        $NumGroupe = $data[3];
+
+        $p = new Personne($id,$Nom,$Prenom);
+        $e = new Eleve($id,$Filiere,$NumGroupe);
+
+        if (!testPInListe($tabP,$p->getiD())) {
             array_push($tabP,$p);
             array_push($tabE,$e);
-           }
-         }
-         $row++;
-         $id++;
+            $id++;
+        }
     }
+
     insertPersonne($connexion,$tabP);
     insertEleve($connexion,$tabE);
     fclose($handle);
 }
 
-function test($Liste,$id)
+function testPInListe($Liste,$id)
 {
   foreach ($Liste as $p)
   {
@@ -43,4 +41,100 @@ function test($Liste,$id)
   return false;
 }
 
+function getCSVforProf($connexion,$filename)
+{
+    $handle = fopen($filename,"r");
+    $tabPersonne = array();
+    $tabPers = array();
+    $tabProf = array();
+    $id = getMaxIDPersonne($connexion)+1;
+    while(($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+    {
+        $Nom = $data[0];
+        $Prenom = $data[1];
+        $NumJury = $data[2];
+
+        $p = new Personne($id,$Nom,$Prenom);
+        $e = new Professeur($id,$NumJury);
+
+        if (!testPInListe($tabPers,$p->getiD())) {
+            array_push($tabPers,$p);
+            array_push($tabProf,$e);
+            $id++;
+        }
+    }
+
+    insertPersonne($connexion,$tabPers);
+    insertProf($connexion,$tabProf);
+    fclose($handle);
+}
+
+function getCSVforGroupe($connexion,$filename)
+{
+    $handle = fopen($filename,"r");
+    $tabGroupe = array();
+    $id = getMaxIDGROUPE($connexion)+1;
+    while(($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+    {
+        $Nom = $data[0];
+        $Lycee = $data[1];
+        $chemin = $data[2];
+
+        $g = new Groupe($id,$Nom,$Lycee,$chemin);
+
+        if (!testPInListeG($tabGroupe,$g->getNumGroupe())) {
+            array_push($tabGroupe,$g);
+            $id++;
+        }
+    }
+
+    insertGroupe($connexion,$tabGroupe);
+    fclose($handle);
+}
+
+function testPInListeG($Liste,$id)
+{
+  foreach ($Liste as $g)
+  {
+    if ($g->getNumGroupe() == $id)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getCSVforJury($connexion,$filename)
+{
+    $handle = fopen($filename,"r");
+    $tabJury = array();
+    $id = getMaxIDJURY($connexion)+1;
+    while(($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+    {
+        $Login = $data[0];
+        $Mdp = $data[1];
+
+        $g = new Jury($id,$Login,$Mdp);
+
+        if (!testPInListeJ($tabJury,$g->getNumJury())) {
+            array_push($tabJury,$g);
+            $id++;
+        }
+    }
+
+    insertJury($connexion,$tabJury);
+    fclose($handle);
+}
+
+function testPInListeJ($Liste,$id)
+{
+  foreach ($Liste as $g)
+  {
+    if ($g->getNumJury() == $id)
+    {
+      return true;
+    }
+  }
+  return false;
+}
 ?>
--- BD/Interactions/InteractionsBD.php
+++ BD/Interactions/InteractionsBD.php
@@ -19,6 +19,46 @@
         }
     }
 
+    function getMaxIDGROUPE($connexion)
+    {
+        // Writing the request
+        $request = "SELECT max(NumGroupe) from GROUPE";
+
+        // Getting the result after the query
+        $result = $connexion->query($request);
+
+        // Fetch the result in var $max
+        $max = $result->fetch();
+
+        // Testing the value of $max
+        if($max['max(NumGroupe)'] > 0){
+            return $max['max(NumGroupe)'];
+        }
+        else{
+            return 0;
+        }
+    }
+
+    function getMaxIDJURY($connexion)
+    {
+        // Writing the request
+        $request = "SELECT max(NumJury) from JURY";
+
+        // Getting the result after the query
+        $result = $connexion->query($request);
+
+        // Fetch the result in var $max
+        $max = $result->fetch();
+
+        // Testing the value of $max
+        if($max['max(NumJury)'] > 0){
+            return $max['max(NumJury)'];
+        }
+        else{
+            return 0;
+        }
+    }
+
     function insertPersonne($connexion,$ListePersonne)
     {
         try{
@@ -92,7 +132,7 @@
                 $Num = $ListeProf[$i]->getNumJury();
 
                 // Execute the insertion
-                $statement.execute();
+                $statement->execute();
             }
         }
         catch(Exception $e)
@@ -118,19 +158,19 @@
 
             $statement->bindParam(':Num',$Num);
             $statement->bindParam(':Nom',$Nom);
-            $statement->bindParam(':Nom',$Lycee);
-            $statement->bindParam(':Nom',$img);
+            $statement->bindParam(':Lycee',$Lycee);
+            $statement->bindParam(':img',$img);
 
             for($i = 0;$i < count($ListeGroupe);$i++)
             {
                 // Passing the value to the parameter
-                $Num = $ListeGroupe[$i]->getNumJury();
+                $Num = $ListeGroupe[$i]->getNumGroupe();
                 $Nom = $ListeGroupe[$i]->getNomProj();
                 $Lycee = $ListeGroupe[$i]->getLycee();
                 $img = $ListeGroupe[$i]->getImageProjet();
 
                 // Execute the insertion
-                $statement.execute();
+                $statement->execute();
             }
         }
         catch(Exception $e)
@@ -158,7 +198,7 @@
             $statement->bindParam(':log',$log);
             $statement->bindParam(':passwd',$pass);
 
-            for($i = 0;$i < count($ListeGroupe);$i++)
+            for($i = 0;$i < count($ListeJury);$i++)
             {
                 // Passing the value to the parameter
                 $Num = $ListeJury[$i]->getNumJury();
@@ -166,7 +206,7 @@
                 $pass = $ListeJury[$i]->getPassword();
 
                 // Execute the insertion
-                $statement.execute();
+                $statement->execute();
             }
         }
         catch(Exception $e)
@@ -276,12 +316,20 @@
             }
             $NB = $reponse->rowCount();
 
-            $Liste["Prototype"] = $Liste["Prototype"] / $NB;
-            $Liste["Originalite"] = $Liste["Originalite"]/$NB;
-            $Liste["DemarcheSI"] = $Liste["DemarcheSI"]/$NB;
-            $Liste["pluriDisciplinarite"] = $Liste["pluriDisciplinarite"]/$NB;
-            $Liste["Maitrise"] = $Liste["Maitrise"]/$NB;
-            $Liste["devDurable"] = $Liste["devDurable"]/$NB;
+            if ($NB == 0)
+            {
+                return array("Prototype"=>0,"Originalite"=> 0,"DemarcheSI"=>0,"pluriDisciplinarite"=>0,"Maitrise"=>0,"devDurable"=>0);
+            }
+            else
+            {
+                $Liste["Prototype"] = $Liste["Prototype"] / $NB;
+                $Liste["Originalite"] = $Liste["Originalite"]/$NB;
+                $Liste["DemarcheSI"] = $Liste["DemarcheSI"]/$NB;
+                $Liste["pluriDisciplinarite"] = $Liste["pluriDisciplinarite"]/$NB;
+                $Liste["Maitrise"] = $Liste["Maitrise"]/$NB;
+                $Liste["devDurable"] = $Liste["devDurable"]/$NB;
+
+            }
 
             return $Liste;
         }
