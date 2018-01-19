fb01e2a6d88f17bf429f7a4cd992c64c6b4df422
ludoliv <olivet@info24-09.iut45.univ-orleans.fr>
2018-01-19 15:42:18 +0100

    ajout des fichiers PHP pour la co a la bd pour android

---
+++ GestionBD_Android/PDOConnection.php
@@ -0,0 +1,8 @@
+﻿<?php
+$dbName = "dbbouny";
+$host = "servinfo-db";
+/*TODO changer les données si installations sur un autre pc*/
+$user = "bouny";
+$pwd = "bouny";
+$cnn = new PDO('mysql:dbname='.$dbName.';host='.$host, $user, $pwd);
+$cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
---
+++ GestionBD_Android/user.php
@@ -0,0 +1,96 @@
+<?php
+include("PDOConnection.php");
+
+//Define some value
+define("ACTION_LOGIN", "login");
+define("RESULT_SUCCESS", 0);
+define("RESULT_ERROR", 1);
+define("RESULT_USER_EXISTS", 2);
+
+
+//$action = $_POST["action"];
+$action="login";
+$result = RESULT_ERROR;
+$jury=array();
+
+if(isset($action))
+{
+	// $username = $_POST["username"];
+	// $pwd = $_POST["password"];
+	$username='Login';
+	$pwd='password';
+	
+	if($action==ACTION_LOGIN) //Action login
+	{
+		if(login($cnn, $username, $pwd))
+		{
+			try{
+				$result = RESULT_SUCCESS;
+				$listeGrp=array();
+				$listeHeure=array();
+				$relation=array();
+				//Login success + envoi bd
+				$sql =  'SELECT DISTINCT * FROM GROUPE NATURAL JOIN JUGE NATURAL JOIN JURY NATURAL JOIN HEURE where login_="'.$username.'" and password_="'.$pwd.'";';
+				$res = $cnn->query($sql);
+
+				while  ($row = $res->fetch()) {
+
+					array_push($listeGrp, array('NumGroupe'=>$row[2],"NomProj"=>$row[3],"Lycee"=>$row[4],"image_Projet"=>$row[5]));
+
+					array_push($relation, array('NumJury'=>$row[1],'NumGroupe'=>$row[2],'idHeure'=>$row[00]));
+
+					array_push($listeHeure, array('idHeure'=>$row[0],'hDeb'=>$row[8],'hFin'=>$row[9]));
+				}
+			}
+			catch(PDOException $e)
+			{
+				echo $e->getMessage();
+			}
+		
+		}
+		else
+		{
+			//login fail
+			$result = RESULT_ERROR;
+		}
+	}
+}
+//Print result as json	
+echo json_encode(array('result' => $result,"Jury"=>$jury,"Groupe"=>$listeGrp,"Heures"=>$listeHeure,"relation"=>$relation));	
+
+function insertUser($cnn, $username, $pwd)
+{
+	$query = "INSERT INTO JURY(login_, password_) VALUES(?, ?)";
+	$stmt = $cnn->prepare($query);
+	$stmt->bindParam(1, $username);
+	$stmt->bindParam(2, $pwd);
+	$stmt->execute();
+}
+function isExistUser($cnn, $username)
+{
+	$query = "SELECT * FROM JURY WHERE login_ = ?";
+	$stmt = $cnn->prepare($query);
+	$stmt->bindParam(1, $username);
+	$stmt->execute();
+	$rowcount = $stmt->rowCount();
+	//for debug
+	//var_dump($rowcount);
+	return $rowcount;
+}
+
+function login($cnn, $username, $pwd)
+{
+	global $jury;
+	$query = "SELECT * FROM JURY WHERE login_ = ? AND password_ = ?";
+	$stmt = $cnn->prepare($query);
+	$stmt->bindParam(1, $username);
+	$stmt->bindParam(2, $pwd);
+	$stmt->execute();
+	while($row=$stmt->fetch()){
+		$jury=array("id"=>$row[0],"Login"=>$row[1],"pwd"=>$row[2]);
+	}
+	$rowcount = $stmt->rowCount();
+	//for debug
+	//var_dump($rowcount);
+	return $rowcount;
+}
