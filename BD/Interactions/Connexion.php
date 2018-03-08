b172d93532b1f363b3b611b035b3d0c02f07ff94
olivet <ludovic111111@hotmail.fr>
2018-03-08 18:59:14 +0100

    changement connexion

--- BD/Interactions/Connexion.php
+++ BD/Interactions/Connexion.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 function connect_database()
 {
@@ -14,21 +13,3 @@ function connect_database()
 }
 
 ?>
-=======
-<?php
-function connect_database()
-{
-  try
-  {
-    // $file_db = new PDO("mysql:dbname=dbbouny;host=servinfo-db", "bouny", "bouny");
-    $file_db = new PDO("mysql:dbname=dbbouny;host=localhost", "bouny", "bouny");
-    $file_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-    return $file_db;
-  }
-  catch(PDOException $e){
-    echo $e->getMessage();
-  }
-}
-
-?>
->>>>>>> glang/master
