845be9c026bcd56d5f11d9736061e5fd8e14f355
Vincent Deschamps <vincent.deschamps@etu.univ-orleans.fr>
2018-03-14 14:49:49 +0100

    requete insertion professeur

---
+++ site-web/templates/admin/insertion_professeur.php
@@ -0,0 +1,31 @@
+<?php
+    require '../Interactions/Connexion.php';
+    require '../Interactions/InteractionsBD.php';
+
+    try{
+        $db = connect_database();
+
+        $ID = getMaxIDPersonne($db)+1;
+        $Nom = $_POST["Name"];
+        $Prenom = $_POST["LastName"];
+        $Jury = $_POST["Jury"];
+
+        $stmt1 = $db->prepare("insert into PERSONNE (ID,Nom,Prenom)values (?,?,?);");
+        $stmt2 = $db->prepare("insert into PROFESSEUR (IDProf,NumJury) values (?,?);");
+
+        $stmt1->bindParam(1,$ID);
+        $stmt1->bindParam(2,$Nom);
+        $stmt1->bindParam(3,$Prenom);
+
+        $stmt2->bindParam(1,$ID);
+        $stmt2->bindParam(2,$Jury);
+
+        $stmt1->execute();
+        $stmt2->execute();
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+
+    header('Location:InsererProfesseur.php');
+?>
