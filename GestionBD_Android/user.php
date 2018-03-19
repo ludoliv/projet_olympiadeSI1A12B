d896cb98d800b5de53fb5d63f52a8b3d89590493
ludoliv <olivet@info27-24.iut45.univ-orleans.fr>
2018-03-19 10:52:02 +0100

    ajout numsalle dans user.php

--- GestionBD_Android/user.php
+++ GestionBD_Android/user.php
@@ -35,7 +35,7 @@ if(isset($action))
 
 				while  ($row = $res->fetch()) {
 
-					array_push($listeGrp, array('NumGroupe'=>$row[2],"NomProj"=>$row[4],"Lycee"=>$row[5],"image_Projet"=>$row[6]));
+					array_push($listeGrp, array('NumGroupe'=>$row[2],"NomProj"=>$row[4],"Lycee"=>$row[5],"image_Projet"=>$row[6],"numSalle"=>$row[3]));
 
 					array_push($relation, array('NumJury'=>$row[1],'NumGroupe'=>$row[2],'idHeure'=>$row[0]));
 
