e9dc1b57d59e5552c1bf31f2d08a94e00a064dd1
BouBBoo <qbouny@gmail.com>
2018-03-20 16:57:51 +0100

    Maj fichier

--- site-web/templates/admin/insert_res.php
+++ site-web/templates/admin/insert_res.php
@@ -9,29 +9,19 @@ $pluridisciplinarite = $_POST['pluridisciplinarite'];
 $maitrise = $_POST['maitrise'];
 $dev_dur = $_POST['developpement_durable'];
 $moyenne = $_POST['moyenne'];
-
-/**
- * Cette fonction met à jour les groupe auxquels sont assignés
- * les récompenses en mettant leur valeurs à NULL.
- * 
- * @author Quentin Bouny
- * 
- * @param PDO $connexion Connexion avec la base de données.
- * 
- */
+// echo $originalite;
+// echo $prototype;
+// echo $demarche_si;
+// echo $pluridisciplinarite;
+// echo $maitrise;
+// echo $dev_dur;
+// echo $moyenne;
 function clear_assignement($connexion)
 {
-  /**
-   * 
-   * @var PDOStatement $stmt Résultat de la requête en base de données.
-   * @var PDOStatement $stmt2 Résultat de la requête en base de données.
-   */
   try{
     $stmt = $connexion->prepare("SELECT idRecompense from RECOMPENSE");
     $stmt2 = $connexion->prepare("UPDATE RECOMPENSE SET idGroupe=NULL where idRecompense=?");
-
     $stmt->execute();
-
     while($row = $stmt->fetch())
     {
       $stmt2->bindParam(1,$row["idRecompense"]);
@@ -43,36 +33,14 @@ function clear_assignement($connexion)
     echo $e->getMessage();
   }
 }
-
-/**
- * Cette fonction permet d'assigner au groupe choisi dans la page resultats_admin.php
- * une récompense.
- * 
- * @author Vincent Deschamps
- * 
- * @param PDO $connexion Connexion avec la base de données.
- * @param Integer $originalite ID du groupe auquel on veut assigner la récompense sur l'originalité.
- * @param Integer $prototype ID du groupe auquel on veut assigner la récompense sur le prototype.
- * @param Integer $demarche_si ID du groupe auquel on veut assigner la récompense sur la démarche SI.
- * @param Integer $pluridisciplinarite ID du groupe auquel on veut assigner la récompense sur la pluri-disciplinarité.
- * @param Integer $maitrise ID du groupe auquel on veut assigner la récompense sur la maitrise.
- * @param Integer $dev_dur ID du groupe auquel on veut assigner la récompense sur le développement durable.
- * @param Integer $moyenne ID du groupe auquel on veut assigner la récompense sur la moyenne générale.
- * 
- * 
- */
 function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridisciplinarite, $maitrise, $dev_dur, $moyenne){
-  /**
-   * @var PDOStatement $stmt Requête préparée à envoyer en base de donées.
-   * @var PDOStatement $stmt2 Requête préparée à envoyer en base de données.
-   */
   try{
     $stmt = $connexion->prepare("UPDATE RECOMPENSE SET idGroupe=? where idRecompense=?");
     $stmt2 = $connexion->prepare("select * from DONNE natural join NOTE where NumGroupe=?");
     if($originalite != "none"){
       $stmt2->bindParam(1,$originalite);
       $stmt2->execute();
-      if(testNote($stmt2,$originalite,"originalite"))
+      if(testNote($connexion,$stmt2,$originalite,"originalite"))
       {
         $stmt->bindParam(1,$originalite);
         $stmt->bindValue(2,1);
@@ -87,7 +55,7 @@ function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridis
     if($prototype != "none"){
       $stmt2->bindParam(1,$prototype);
       $stmt2->execute();
-      if(testNote($stmt2,$prototype,"prototype"))
+      if(testNote($connexion,$stmt2,$prototype,"prototype"))
       {
       $stmt->bindParam(1,$prototype);
       $stmt->bindValue(2,2);
@@ -102,7 +70,7 @@ function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridis
     if($demarche_si != "none"){
       $stmt2->bindParam(1,$demarche_si);
       $stmt2->execute();
-      if(testNote($stmt2,$demarche_si,"DemarcheScientifique"))
+      if(testNote($connexion,$stmt2,$demarche_si,"DemarcheScientifique"))
       {
         $stmt->bindParam(1,$demarche_si);
         $stmt->bindValue(2,3);
@@ -117,7 +85,7 @@ function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridis
     if($pluridisciplinarite != "none"){
       $stmt2->bindParam(1,$pluridisciplinarite);
       $stmt2->execute();
-      if(testNote($stmt2,$pluridisciplinarite,"pluriDisciplinarite"))
+      if(testNote($connexion,$stmt2,$pluridisciplinarite,"pluriDisciplinarite"))
       {
         $stmt->bindParam(1,$pluridisciplinarite);
         $stmt->bindValue(2,4);
@@ -132,7 +100,7 @@ function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridis
     if($maitrise != "none"){
       $stmt2->bindParam(1,$maitrise);
       $stmt2->execute();
-      if(testNote($stmt2,$maitrise,"MaitriseScientifique"))
+      if(testNote($connexion,$stmt2,$maitrise,"MaitriseScientifique"))
       {
         $stmt->bindParam(1,$maitrise);
         $stmt->bindValue(2,5);
@@ -147,7 +115,7 @@ function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridis
     if($dev_dur != "none"){
       $stmt2->bindParam(1,$dev_dur);
       $stmt2->execute();
-      if(testNote($stmt2,$dev_dur,"Communication"))
+      if(testNote($connexion,$stmt2,$dev_dur,"Communication"))
       {
         $stmt->bindParam(1,$dev_dur);
         $stmt->bindValue(2,6);
@@ -161,7 +129,7 @@ function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridis
     }
     if($moyenne != "none"){
       $note = getNote($connexion,$moyenne);
-      if(($note["Prototype"] > 0) and ($note["Originalite"] > 0) and ($note["DemarcheSI"] > 0) and ($note["pluriDisciplinarite"] > 0) and ($note["devDurable"] > 0) and ($note["Maitrise"]>0))
+      if(($note["Prototype"] >= 0) and ($note["Originalite"] >= 0) and ($note["DemarcheSI"] >= 0) and ($note["pluriDisciplinarite"] >= 0) and ($note["devDurable"] >= 0) and ($note["Maitrise"]>=0))
       {
         $stmt->bindParam(1,$moyenne);
         $stmt->bindValue(2,7);
@@ -181,20 +149,37 @@ function insertion($connexion, $originalite, $prototype, $demarche_si, $pluridis
 clear_assignement($database);
 insertion($database, $originalite, $prototype, $demarche_si, $pluridisciplinarite, $maitrise, $dev_dur, $moyenne);
 header('Location: resultats_admin.php');
-function testNote($statement,$note,$categorie){
-  try
-  {
+function testNote($connexion,$statement,$grp,$categorie){
+  /**
+   * @author : Quentin Bouny
+   */
+  try{
+    $stmt = $connexion->prepare("SELECT Lycee FROM GROUPE WHERE NumGroupe in (SELECT idGroupe from RECOMPENSE)");
     while($row = $statement->fetch())
     {
-      if($row[$categorie] > 0)
+      if($row[$categorie] >= 0)
       {
-        return true;
+        if ($categorie === "originalite")
+        {
+          return true;
+        }
+        echo "Groupe:".$grp."<br>";
+        $stmt->execute();
+        $cpt = 0;
+        while ($row = $stmt->fetch())
+        {
+          $cpt++;
+        }
+        if($cpt < 2)
+        {
+          return true;
+        }
       }
-      return false;
     }
+    return false;
   }
   catch(Exception $e){
-    echo $e->getMessage();
   }
 }
 ?>
+
