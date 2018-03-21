a70052bd58264530fe9181ffe7466dab044bafda
ludoliv <olivet@info27-23.iut45.univ-orleans.fr>
2018-03-21 10:28:25 +0100

    ajout d'un jeu d'essai plus conséquent

--- BD/Interactions/Connexion.php
+++ BD/Interactions/Connexion.php
@@ -2,21 +2,21 @@
 
 /**
  * Cette fonction permet de se connecter à la base de données.
- * 
+ *
  * @author Quentin Bouny
- * 
+ *
  * @return PDO  Retourne la connexion avec la base de données.
  */
 function connect_database()
 {
   /**
-   * 
+   *
    * @var PDO $file_db Connexion avec la base de données.
    */
   try
   {
-    // $file_db = new PDO("mysql:dbname=dbbouny;host=servinfo-db", "bouny", "bouny");
-    $file_db = new PDO("mysql:dbname=dbbouny;host=localhost", "bouny", "bouny");
+    $file_db = new PDO("mysql:dbname=dbbouny;host=servinfo-db", "bouny", "bouny");
+    // $file_db = new PDO("mysql:dbname=dbbouny;host=localhost", "bouny", "bouny");
     // $file_db = new PDO("mysql:dbname=dbbouny;host=localhost", "root", "");
     $file_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
     return $file_db;
--- BD/Script/Script_JeuEssai.sql
+++ BD/Script/Script_JeuEssai.sql
@@ -3,36 +3,48 @@ insert into PERSONNE values(1,"COUCKE","Maxime");
 insert into PERSONNE values(2,"CAMA","Thang-Long");
 insert into PERSONNE values(3,"TAMIN","Antoine");
 insert into PERSONNE values(4,"FAURE","Nathan");
+insert into PERSONNE values(5,"OLIVET","Ludovic");
+insert into PERSONNE values(6,"DESCHAMPS","Vincent");
+insert into PERSONNE values(7,"DUBOIS","Florian");
+insert into PERSONNE values(8,"SCHULTZ","Martin");
+insert into PERSONNE values(9,"GRILLET","Lionel");
 
 insert into GROUPE values(1,"Stopwatch","Lycee de l'heure",1,"None");
 insert into GROUPE values(2,"Ariane","Lycee de la lune",2,"None");
 insert into GROUPE values(3,"Animaux","Lycee Pierre Bouillet",3,"None");
-insert into JURY values(1,"Login","MDP");
-insert into JURY values(2,"Login","password");
+insert into GROUPE values(4,"Fraise","Lycee Jean Bernard",1,"None");
+insert into GROUPE values(5,"Jardin","Lycee de la lune",2,"None");
+insert into GROUPE values(6,"Aigle","Lycee Pierre Bouillet",3,"None");
+
+insert into JURY values(1,"jury1","jury1");
+insert into JURY values(2,"jury2","jury2");
 
 insert into HEURE values(1,'9:00','9:20');
 insert into HEURE values(2,'9:40','10:00');
 insert into HEURE values(3,'10:00','10:20');
+insert into HEURE values(4,'10:40','11:00');
+insert into HEURE values(5,'11:40','12:00');
 
 insert into JUGE values(1,1,1,1);
 insert into JUGE values(2,1,2,2);
 insert into JUGE values(3,1,3,3);
+insert into JUGE values(3,2,1,1);
+insert into JUGE values(4,2,2,2);
+insert into JUGE values(5,2,3,3);
 
-insert into ELEVE values(4,"STI2D",1);
-insert into ELEVE values(3,"STI2D",1);
-insert into ELEVE values(2,"STI2D",2);
-
+insert into ELEVE values(2,"S",1);
+insert into ELEVE values(3,"S",2);
+insert into ELEVE values(4,"S",3);
+insert into ELEVE values(5,"STI2D",4);
+insert into ELEVE values(6,"STI2D",5);
+insert into ELEVE values(7,"STI2D",6);
 
 insert into PROFESSEUR values(0,1);
 insert into PROFESSEUR values(1,1);
+insert into PROFESSEUR values(8,2);
+insert into PROFESSEUR values(9,2);
 
-insert into NOTE values(1,2,2,2,2,2,2);
-insert into DONNE values(1,1,1);
-
-insert into NOTE values(2,5,4,3,4,5,2);
-insert into DONNE values(2,1,2);
-
-insert into ADMINISTRATEUR values("Log","Passwd");
+insert into ADMINISTRATEUR values("admin","admin");
 
 insert into RECOMPENSE values (1,NULL,"Originalite");
 insert into RECOMPENSE values (2,NULL,"Prototype");
@@ -42,4 +54,4 @@ insert into RECOMPENSE values (5,NULL,"MaitriseScientifique");
 insert into RECOMPENSE values (6,NULL,"Communication");
 insert into RECOMPENSE values(7,NULL,"moyenne");
 
-insert into OLYMPIADES values(1,null,null,null,null,null,null);
+insert into OLYMPIADES values(1,"base.png","base.png","base.png",NULL,"base.png","base.png");
