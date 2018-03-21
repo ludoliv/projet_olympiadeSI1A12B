338ceb03e66760f18382dfccd543c98439bcaccd
Thang-long <thang-long@hotmail.fr>
2018-03-21 17:00:28 +0100

    Bootstrap 3 to Bootstrap 4

--- site-web/templates/admin/InsererHoraires.php
+++ site-web/templates/admin/InsererHoraires.php
@@ -12,88 +12,108 @@ $_SESSION['connect']=0;
 if(!isset($_SESSION['loginOK'])){
   header('Location: ../protection/connexion.php');
 }?>
-<div style="display:flex">
-  <?php include 'menu_admin.php'; ?>
+<?php include 'menu_admin.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Élèves
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="InsererEleveCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererEleve.php">Créer</a>
+        </div>
+      </li>
 
-  <nav class="navbar navbar-expand-lg navbar-light navbar-right" style="margin-left: 11%">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Professeurs
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="InsererProfesseurCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererProfesseur.php">Créer</a>
+        </div>
+      </li>
 
-    <div class="collapse navbar-collapse" id="navbar">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Élèves
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="InsererEleveCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererEleve.php">Créer</a>
-          </div>
-        </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Groupes
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="InsererGroupeCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererGroupe.php">Créer</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Jurys
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="insererJuryCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="insererJury.php">Créer</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Horaires
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="insererHorairesCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererHoraires.php">Créer</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Professeurs
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="InsererProfesseurCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererProfesseur.php">Créer</a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Groupes
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="InsererGroupeCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererGroupe.php">Créer</a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Jurys
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="insererJuryCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="insererJury.php">Créer</a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Horaires
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="insererHorairesCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererHoraires.php">Créer</a>
-          </div>
-        </li>
-      </ul>
+<div id="main" class="container-fluid" style="height:50%">
+  <div class="row justify-content-between" style="height:150%">
+    <div class="col-8">
+      <form name="AjoutHoraires" method="POST" style="padding-top: 2%" action="insertion_horaires.php">
+        <h4>Formulaire d'ajout d'un horaire :</h4>
+        <div class="form-group row">
+          <label for="HeureDeb" class="col-4">Début</label>
+          <input id="HeureDeb" type="time" name="HeureDeb" value="08:00" required></input>
+        </div>
+        <div class="form-group row">
+          <label for="HeureFin" class="col-4">Fin</label>
+          <input id="HeureFin" type="time" name="HeureFin" value="08:20" required></input>
+        </div>
+        <div class="text-center">
+          <button class="btn btn-dark" type="submit">Ajouter Horaire</button>
+        </div>
+      </form>
     </div>
+    <div class="col-4" style="overflow-y:scroll; height:100%; width: 400px">
+      <?php
+      require "../../../BD/Interactions/Connexion.php";
+      require "../../../BD/Interactions/InteractionsBD.php";
+      $db = connect_database();
+      try{
+        $stmt2 = $db->prepare("SELECT * FROM HEURE order by hDeb");
+        $stmt2->execute();
+        echo '<div class="jumbotron text-white bg-dark" style="padding:1em">';
+        while($row = $stmt2->fetch())
+        {
+          echo '
+            <div class="jumbotron text-dark bg-light" style="padding:0.5em">
+              <p>Début :'.$row["hDeb"].'</p>
+              <p>Fin : '.$row["hFin"].'</p>
+            </div>';
+          }
+        echo '</div>';
+      }
+      catch(Exception $e)
+      {
+        echo $e->getMessage();
+      }
 
-  </nav>
-</div>
+      ?>
 
-<form name="AjoutHoraires" method="POST" style="padding-top: 2%" action="insertion_horaires.php">
-  <h4>Formulaire d'ajout d'un horaire :</h4>
-  <table>
-    <tr>
-      <td style="width: 300px">
-        <label style="padding-left: 2% ; padding-right: 2% ; padding-top: 2%">Heure de début :</label>
-      </td>
-      <td>
-        <input type="time" name="HeureDeb" value="08:00"required></input>
-      </td>
-    </tr>
-    <tr>
-      <td style="width: 300px">
-        <label style="padding-left: 2% ; padding-right: 2% ; padding-top: 2%">Heure de fin :</label>
-      </td>
-      <td>
-        <input type="time" name="HeureFin" value="08:20"required></input>
-      </td>
-    </tr>
-    </table>
-    <input type="submit" value="Ajouter horaire">
-</form>
+    </div>
+  </div>
 </body>
--- site-web/templates/admin/insererHorairesCSV.php
+++ site-web/templates/admin/insererHorairesCSV.php
@@ -12,81 +12,145 @@ $_SESSION['connect']=0;
 if(!isset($_SESSION['loginOK'])){
   header('Location: ../protection/connexion.php');
 }?>
-<div style="display:flex">
-  <?php include 'menu_admin.php'; ?>
+<?php include 'menu_admin.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Élèves
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="InsererEleveCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererEleve.php">Créer</a>
+        </div>
+      </li>
 
-  <nav class="navbar navbar-expand-lg navbar-light navbar-right" style="margin-left: 11%">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Professeurs
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="InsererProfesseurCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererProfesseur.php">Créer</a>
+        </div>
+      </li>
 
-    <div class="collapse navbar-collapse" id="navbar">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Élèves
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="InsererEleveCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererEleve.php">Créer</a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Professeurs
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="InsererProfesseurCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererProfesseur.php">Créer</a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Groupes
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="InsererGroupeCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererGroupe.php">Créer</a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Jurys
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="insererJuryCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="insererJury.php">Créer</a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Horaires
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="insererHorairesCSV.php">Import CSV</a>
-            <a class="dropdown-item" href="InsererHoraires.php">Créer</a>
-          </div>
-        </li>
-      </ul>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Groupes
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="InsererGroupeCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererGroupe.php">Créer</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Jurys
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="insererJuryCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="insererJury.php">Créer</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Horaires
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="insererHorairesCSV.php">Import CSV</a>
+          <a class="dropdown-item" href="InsererHoraires.php">Créer</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div id="main" class="container-fluid" style="height:50%">
+  <div class="row justify-content-between" style="height:150%">
+    <div class="col-8">
+      <form name="AjoutEleve" method="POST" style="padding-top: 2%" action="insertion_horaires_CSV.php" enctype="multipart/form-data">
+        <h4>Formulaire d'ajout d'un horaire (CSV) :</h4>
+        <div class="form-group row">
+          <label for="filename" class="col-4">Fichier CSV</label>
+          <input id="filename" type="file" name="file" required></input>
+        </div>
+        <div class="text-center">
+          <button class="btn btn-dark" type="submit">Ajouter Horaire</button>
+        </div>
+      </form>
     </div>
+    <div class="col-4" style="overflow-y:scroll; height:100%; width: 400px">
+      <?php
+      require "../../../BD/Interactions/Connexion.php";
+      require "../../../BD/Interactions/InteractionsBD.php";
+      $db = connect_database();
+      try{
+        $maxNumGroupe = getMaxIDGROUPE($db);
+        $grpancien = 0;
+        $stmt2 = $db->prepare("SELECT * FROM ELEVE natural join PERSONNE natural join GROUPE where ID=IDEleve and NumGroupe = ?");
+        $stmt = $db->prepare("SELECT * FROM GROUPE where NumGroupe not in(SELECT NumGroupe FROM ELEVE)");
+        $stmt->execute();
 
-  </nav>
-</div>
+        for ($i=1; $i<=$maxNumGroupe; $i++){
+          $stmt2->bindParam(1, $i);
+          $stmt2->execute();
+          while($row = $stmt2->fetch())
+          {
+            if($row["NumGroupe"] != $grpancien){
+              if($i != 1){
+                echo '</div>';
+              }
+              echo '
+              <div id="grp-'.$row["NumGroupe"].'" class="jumbotron text-white bg-dark" style="padding:1em">
+                <h1>Groupe n°'.$row["NumGroupe"].'</h1>
+                <h2>Projet : '.$row["NomProjet"].'</h2>
+                <div class="jumbotron text-dark bg-light" style="padding:0.5em">
+                  <p>Élève n°'.$row["IDEleve"].'</p>
+                  <p>Nom : '.$row["Nom"].' '.$row["Prenom"].'</p>
+                </div>';
+              }
+              else{
+                echo '
+                <div class="jumbotron text-dark bg-light" style="padding:0.5em">
+                  <p>Élève n°'.$row["IDEleve"].'</p>
+                  <p>Nom : '.$row["Nom"].' '.$row["Prenom"].'</p>
+                </div>
+                ';
+              }
+              $grpancien = $row["NumGroupe"];
+            }
+          }
+          echo "</div>";
+          while($row2 = $stmt->fetch()){
+            if($i != $maxNumGroupe-1){
+              echo '
+              <div id="grp-'.$row2["NumGroupe"].'" class="jumbotron text-white bg-dark" style="padding:1em">
+                <h1>Groupe n°'.$row2["NumGroupe"].'</h1>
+                <h2>Projet : '.$row2["NomProjet"].'</h2>
+              </div>';
+            }
+            else{
+              echo '
+              </div>
+              <div id="grp-'.$row2["NumGroupe"].'" class="jumbotron text-white bg-dark" style="padding:1em">
+                <h1>Groupe n°'.$row2["NumGroupe"].'</h1>
+                <h2>Projet : '.$row2["NomProjet"].'</h2>
+              </div>';
+            }
+        }
+      }
+      catch(Exception $e)
+      {
+        echo $e->getMessage();
+      }
 
-<form name="AjoutJury" method="POST" style="padding-top: 2%" action="insertion_horaires_CSV.php" enctype="multipart/form-data">
-  <h4>Formulaire d'ajout d'un horaire :</h4>
-  <table>
-    <tr>
-      <td style="width: 300px">
-        <label style="padding-left: 2% ; padding-right: 2% ; padding-top: 2%">Fichier CSV</label>
-      </td>
-      <td>
-        <input id="filename" type="file" name="file" required></input>
-      </td>
-    </tr>
-    </table>
-    <input type="submit" value="Ajouter horaire">
-</form>
+      ?>
+
+    </div>
+  </div>
 
 </body>
