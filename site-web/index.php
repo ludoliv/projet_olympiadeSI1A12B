ce08065ca3701d43e0ec82f21faff6d66f30b019
Vincent Deschamps <vincent.deschamps@etu.univ-orleans.fr>
2018-03-22 11:24:53 +0100

    accueil propre

--- site-web/index.php
+++ site-web/index.php
@@ -9,21 +9,33 @@
 </head>
 <body class="h-100">
   <?php
-    include '../BD/Interactions/Connexion.php';
-    $db = connect_database();
+  include '../BD/Interactions/Connexion.php';
+  $db = connect_database();
 
-    $statement = $db->prepare("SELECT * from OLYMPIADES");
-    $statement->execute();
+  $statement = $db->prepare("SELECT * from OLYMPIADES");
+  $statement->execute();
 
-    while($row = $statement->fetch()){
-      $numEdition = $row["NumEdition"];
-      $LogOlympiades = $row["LogOlympiades"];
-      $LogoSponsor = $row["LogoSponsor"];
-      $LogoUPSTI = $row["LogoUPSTI"];
-      $LogoIUT = $row["LogoIUT"];
-      $date = $row["datetimeOlymp"];
-    }
+  while($row = $statement->fetch()){
+    $numEdition = $row["NumEdition"];
+    $LogOlympiades = $row["LogOlympiades"];
+    $LogoSponsor = $row["LogoSponsor"];
+    $LogoUPSTI = $row["LogoUPSTI"];
+    $LogoIUT = $row["LogoIUT"];
+    $dateBD = $row["datetimeOlymp"];
+    $bandeau = $row["BandeauPartenaires"];
+
+  }
+
+  $lesMois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+
+  $annee = explode("-", $dateBD)[0];
+  $mois = $lesMois[intval(explode("-", $dateBD)[1])];
+  $jour = explode(" ", explode("-", $dateBD)[2])[0];
+
+  $date = $jour." ".$mois." ".$annee;
+
   ?>
+
   <div class="container-fluid h-100" style="padding-bottom: 13%">
 
     <div class="row justify-content-start">
@@ -37,60 +49,59 @@
         <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
       </div>
 
-      <div class="col-8 text-center">
-        <h1 class="display-2"><?php echo "Olympiades n°".$numEdition?></h1>
+      <div class="col-2 media" id="sponsor">
+        <img style="height:100px" src="<?php
+        echo '../../images/'.$LogoSponsor;
+        ?>"
+        />
+      </div>
+
+      <div class="col-4" style="text-align:center">
+        <h1><?php echo "Olympiades n°".$numEdition;?></h1>
+        <h5 id="date"><?php echo $date;?></h5>
       </div>
 
       <div class="col-2">
         <div class="media" id="upsti">
-          <img src="<?php
-            echo '../../images/'.$LogoUPSTI;
+          <img  style="height:100px" src="<?php
+          echo '../../images/'.$LogoUPSTI;
           ?>"/>
         </div>
       </div>
-    </div>
 
-    <div class="row justify-content-center h-100">
-      <div class="col align-self-center">
-        <div class="media" id="olymp">
-          <img src="<?php
-            echo '../../images/'.$LogOlympiades;
-          ?>"/>
-        </div>
-      </div>
-    </div>
-
-    <div class="row justify-content-end">
-      <div class="col-4">
-        <div class="media" id="upsti">
-          <img src="<?php
-            echo '../../images/'.$LogoSponsor;
-          ?>"/>
-        </div>
-      </div>
-      <div class="col-4 text-center align-self-center">
-        <h5 id="date"><?php echo $date?></h5>
-      </div>
-      <div class="col-4">
+      <div class="col-2">
         <div class="media" id="iut">
-          <img src="<?php
-            echo '../../images/'.$LogoIUT;
+          <img style="height:100px" src="<?php
+          echo '../../images/'.$LogoIUT;
           ?>"/>
         </div>
       </div>
     </div>
+
+    <div class="row justify-content-center">
+      <div class="media" id="olymp">
+        <img style="height:500px" src="<?php
+        echo '../../images/'.$LogOlympiades;
+        ?>"/>
+      </div>
+    </div>
+
+    <div class="media" id="bandeau">
+      <img style="height:100px; display: block; margin-left: auto; margin-right: auto;" src="<?php
+      echo '../../images/'.$bandeau;
+      ?>"/>
+    </div>
+
 
   </div>
 
   <script>
-  let elemdate = document.getElementById("date").textContent.split(" ")[0].split("-");
-  document.getElementById("date").textContent = elemdate[2]+"-"+elemdate[1]+"-"+elemdate[0];
   function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
   }
 
   function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
   }
   </script>
 </body>
--- site-web/templates/jury/accueil_jury.php
+++ site-web/templates/jury/accueil_jury.php
@@ -2,7 +2,9 @@
 <html>
 <head>
 <link rel="stylesheet" href="../../css/index.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 </head>
 <body>
   <?php session_start();
@@ -12,20 +14,76 @@
   }?>
   <?php include "menu_jury.php"?>
 <center><h1 class="vignets">Bienvenue, jury</h1></center>
+<div class="container">
+  <div class="carousel slide bg-dark" data-ride="carousel" style="padding-top:1em ; padding-bottom: 1em">
+    <h1 class="text-center text-white">Projets de cette année</h1>
+    <div class="carousel-inner text-center">
+      <?php
+      require "../../../BD/Interactions/Connexion.php";
+      require "../../../BD/Interactions/InteractionsBD.php";
+      $db = connect_database();
+      try{
+        $cpt = 0;
+        $stmt = $db->prepare("SELECT * from GROUPE where image_Projet != 'None'");
+        $stmt->execute();
+        while($row = $stmt->fetch()){
+          if($cpt == 0){
+            echo '
+            <div class="carousel-item active">
+              <img src="../../images_projets/'.$row["image_Projet"].'" height="500px">
+              <br>
+              <div class=bg-light style=margin-top:15px>
+                <h5>'.strtoupper($row["NomProjet"][0]).substr($row["NomProjet"],1,strlen($row["NomProjet"])+1).'</h5>
+                <p>'.strtoupper($row["Lycee"][0]).substr($row["Lycee"],1,strlen($row["Lycee"])+1).'</p>
+              </div>
+            </div>
+            ';
+            $cpt++;
+          }
+          else{
+            echo '
+            <div class="carousel-item">
+              <img src="../../images_projets/'.$row["image_Projet"].'" height="500px">
+              <div class=bg-light style=margin-top:15px;>
+                <h5>'.strtoupper($row["NomProjet"][0]).substr($row["NomProjet"],1,strlen($row["NomProjet"])+1).'</h5>
+                <p>'.strtoupper($row["Lycee"][0]).substr($row["Lycee"],1,strlen($row["Lycee"])+1).'</p>
+              </div>
+            </div>
+            ';
+          }
+        }
+      }
+      catch(Exception $e){
+        $e->getMessage();
+      }
+      ?>
+    </div>
+    <a id="prev" class="carousel-control-prev" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a id="next" class="carousel-control-next" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<script type="text/javascript">
+$(document).ready(function(){
+  $(".carousel").carousel({
+    interval: 10000
+  });
+  $("#titre").addClass("load");
+  document.getElementById("prev").onclick = function(){$(".carousel").carousel('prev')}
+  document.getElementById("next").onclick = function(){$(".carousel").carousel('next')}
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+  }
 
-<script>
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-$(function() {
-      $(".vignets").addClass("load");
+  function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+  }
 });
 </script>
-
 </body>
 </html>
